33f7579de080ad92aab2020817b5106200c63ffa
AlexFordnirs <71636706+AlexFordnirs@users.noreply.github.com>
2024-02-23 19:08:46 +0200

    create forms test

---
+++ Forms/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Forms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(int.Parse(Properties.Resources.WidthMain), int.Parse(Properties.Resources.HeigthMain));
+            this.Text = Properties.Resources.MainFormName;
+            this.BackColor = Color.Black;
+            this.ForeColor = Color.Aqua;
+            //
+            // Init Elements
+            //
+            X = new TextBox();
+            Y = new TextBox();
+            SizeW = new TextBox();
+            SizeH = new TextBox();
+            NameInput = new TextBox();
+            ColorText = new TextBox();
+            NewModalBtn = new Button();
+            NewNoneModalBtn = new Button();
+            //
+            // X , Y 
+            //
+            X.Location = new System.Drawing.Point(int.Parse(Properties.Resources.TextXLocX), int.Parse(Properties.Resources.TextXLocY));
+            X.Text = Properties.Resources.XText;
+            Y.Location = new System.Drawing.Point(int.Parse(Properties.Resources.TextYLocX), int.Parse(Properties.Resources.TextYLocY));
+            Y.Text = Properties.Resources.YText;
+            //
+            // Width , Heigth
+            //
+            SizeW.Location = new Point(int.Parse(Properties.Resources.SizeWX), int.Parse(Properties.Resources.SizeWY));
+            SizeW.Text = Properties.Resources.SizeWText;
+            SizeH.Location = new Point(int.Parse(Properties.Resources.SizeHX), int.Parse(Properties.Resources.SizeHY));
+            SizeH.Text = Properties.Resources.SizeHText;
+            //
+            // Name , Color
+            //
+            NameInput.Location = new Point(int.Parse(Properties.Resources.NameInputX), int.Parse(Properties.Resources.NameInputY));
+            NameInput.Text = Properties.Resources.NameInputText;
+            ColorText.Location = new Point(int.Parse(Properties.Resources.ColorTextX), int.Parse(Properties.Resources.ColorTextY));
+            ColorText.Text = Properties.Resources.ColorTxt;
+            //
+            // ButtonNewForm
+            //
+            NewModalBtn.Location = new Point(int.Parse(Properties.Resources.ModalBtnX), int.Parse(Properties.Resources.ModalBtnY));
+            NewModalBtn.Size = new Size(int.Parse(Properties.Resources.ModalBtnW), int.Parse(Properties.Resources.ModalBtnH));
+            NewModalBtn.Text = Properties.Resources.ModalBtnText;
+            //
+            // ButtonNoneModalBtn
+            //
+            NewNoneModalBtn.Location = new Point(int.Parse(Properties.Resources.NoneModalBtnX), int.Parse(Properties.Resources.NoneModalBtnY));
+            NewNoneModalBtn.Size = new Size(int.Parse(Properties.Resources.NoneModalBtnW), int.Parse(Properties.Resources.NoneModalBtnH));
+            NewNoneModalBtn.Text = Properties.Resources.NoneModalBtnText;
+            //
+            // Clicks
+            //
+            NewModalBtn.MouseClick += NewModalFormClick;
+            NewNoneModalBtn.MouseClick += NewNoneModalFormClick;
+            //
+            // Controls
+            //
+            Controls.Add(X);
+            Controls.Add(Y);
+            Controls.Add(SizeW);
+            Controls.Add(SizeH);
+            Controls.Add(NameInput);
+            Controls.Add(ColorText);
+            Controls.Add(NewModalBtn);
+            Controls.Add(NewNoneModalBtn);
+
+        }
+
+
+        TextBox X, Y;
+        TextBox SizeW, SizeH;
+        TextBox NameInput, ColorText;
+
+        Button NewModalBtn;
+        Button NewNoneModalBtn;
+
+        #endregion
+    }
+}
+
---
+++ Forms/Form1.cs
@@ -0,0 +1,242 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Forms
+{
+    public partial class Form1 : Form
+    {
+
+        string Errors = "";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void NewModalFormClick(object sender, MouseEventArgs e)
+        {
+            Form Form2 = new Form();
+            NameInput.BackColor = Color.White;
+            X.BackColor = Color.White;
+            Y.BackColor = Color.White;
+            SizeW.BackColor = Color.White;
+            SizeH.BackColor = Color.White;
+            Errors = "";
+            try
+            {
+                if (NameInput.Text == Properties.Resources.NameInputText)
+                {
+                    throw new InvalidOperationException("Incorrect Name");
+                }
+                else
+                {
+                    Form2.Text = NameInput.Text;
+                    NameInput.BackColor = Color.White;
+                }
+                Form2.Size = new Size(int.Parse(SizeW.Text), int.Parse(SizeH.Text));
+                Form2.Location = new Point(int.Parse(X.Text), int.Parse(Y.Text));
+
+                Color color;
+                try
+                {
+                    color = Color.FromName(ColorText.Text);
+                    Form2.BackColor = color == null ? Color.Red : color;
+                    ColorText.BackColor = Color.White;
+                    Form2.FormBorderStyle = FormBorderStyle.Fixed3D;
+                    Form2.ShowDialog();
+                }
+                catch (Exception ex)
+                {
+                    Errors += "Incorrect Color" + Environment.NewLine;
+                    ColorText.BackColor = Color.Red;
+                    MessageBox.Show(Errors, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                }
+
+            }
+            catch
+            {
+                bool isInt;
+                int res;
+
+                if (NameInput.Text == "" || NameInput.Text == Properties.Resources.NameInputText)
+                {
+                    NameInput.BackColor = Color.Red;
+                    Errors += "Incorrect Name" + Environment.NewLine;
+                }
+                else
+                {
+                    NameInput.BackColor = Color.White;
+                }
+                isInt = Int32.TryParse(SizeW.Text, out res);
+                if (SizeW.Text == "" || SizeW.Text == Properties.Resources.SizeWText || isInt == false)
+                {
+                    SizeW.BackColor = Color.Red;
+                    Errors += "Incorrect Width" + Environment.NewLine;
+                }
+                else
+                {
+                    SizeW.BackColor = Color.White;
+                }
+                isInt = Int32.TryParse(SizeH.Text, out res);
+                if (SizeH.Text == "" || SizeH.Text == Properties.Resources.SizeHText || isInt == false)
+                {
+                    SizeH.BackColor = Color.Red;
+                    Errors += "Incorrect Heigth" + Environment.NewLine;
+                }
+                else
+                {
+                    SizeH.BackColor = Color.White;
+                }
+                isInt = Int32.TryParse(X.Text, out res);
+                if (X.Text == "" || X.Text == Properties.Resources.XText || isInt == false)
+                {
+                    X.BackColor = Color.Red;
+                    Errors += "Incorrect X" + Environment.NewLine;
+                }
+                else
+                {
+                    X.BackColor = Color.White;
+                }
+                isInt = Int32.TryParse(Y.Text, out res);
+                if (Y.Text == "" || Y.Text == Properties.Resources.YText || isInt == false)
+                {
+                    Y.BackColor = Color.Red;
+                    Errors += "Incorrect Y" + Environment.NewLine;
+                }
+                else
+                {
+                    Y.BackColor = Color.White;
+                }
+                if (ColorText.Text == "" || ColorText.Text == Properties.Resources.ColorTxt)
+                {
+                    ColorText.BackColor = Color.Red;
+                    Errors += "Incorrect Color";
+                }
+                else
+                {
+                    ColorText.BackColor = Color.White;
+                }
+                MessageBox.Show(Errors, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+
+            }
+            GC.Collect();
+        }
+        private void NewNoneModalFormClick(object sender, MouseEventArgs e)
+        {
+            Form Form2 = new Form();
+            NameInput.BackColor = Color.White;
+            X.BackColor = Color.White;
+            Y.BackColor = Color.White;
+            SizeW.BackColor = Color.White;
+            SizeH.BackColor = Color.White;
+            Errors = "";
+            try
+            {
+                if (NameInput.Text == Properties.Resources.NameInputText)
+                {
+                    throw new InvalidOperationException("Incorrect Name");
+                }
+                else
+                {
+                    Form2.Text = NameInput.Text;
+                    NameInput.BackColor = Color.White;
+                }
+                Form2.Size = new Size(int.Parse(SizeW.Text), int.Parse(SizeH.Text));
+                Form2.Location = new Point(int.Parse(X.Text), int.Parse(Y.Text));
+
+                Color color;
+                try
+                {
+                    color = Color.FromName(ColorText.Text);
+                    Form2.BackColor = color == null ? Color.Red : color;
+                    ColorText.BackColor = Color.White;
+                    Form2.FormBorderStyle = FormBorderStyle.Fixed3D;
+                    Form2.Show();
+                }
+                catch (Exception ex)
+                {
+                    Errors += "Incorrect Color" + Environment.NewLine;
+                    ColorText.BackColor = Color.Red;
+                    MessageBox.Show(Errors, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                }
+
+            }
+            catch
+            {
+                bool isInt;
+                int res;
+
+                if (NameInput.Text == "" || NameInput.Text == Properties.Resources.NameInputText)
+                {
+                    NameInput.BackColor = Color.Red;
+                    Errors += "Incorrect Name" + Environment.NewLine;
+                }
+                else
+                {
+                    NameInput.BackColor = Color.White;
+                }
+                isInt = Int32.TryParse(SizeW.Text, out res);
+                if (SizeW.Text == "" || SizeW.Text == Properties.Resources.SizeWText || isInt == false)
+                {
+                    SizeW.BackColor = Color.Red;
+                    Errors += "Incorrect Width" + Environment.NewLine;
+                }
+                else
+                {
+                    SizeW.BackColor = Color.White;
+                }
+                isInt = Int32.TryParse(SizeH.Text, out res);
+                if (SizeH.Text == "" || SizeH.Text == Properties.Resources.SizeHText || isInt == false)
+                {
+                    SizeH.BackColor = Color.Red;
+                    Errors += "Incorrect Heigth" + Environment.NewLine;
+                }
+                else
+                {
+                    SizeH.BackColor = Color.White;
+                }
+                isInt = Int32.TryParse(X.Text, out res);
+                if (X.Text == "" || X.Text == Properties.Resources.XText || isInt == false)
+                {
+                    X.BackColor = Color.Red;
+                    Errors += "Incorrect X" + Environment.NewLine;
+                }
+                else
+                {
+                    X.BackColor = Color.White;
+                }
+                isInt = Int32.TryParse(Y.Text, out res);
+                if (Y.Text == "" || Y.Text == Properties.Resources.YText || isInt == false)
+                {
+                    Y.BackColor = Color.Red;
+                    Errors += "Incorrect Y" + Environment.NewLine;
+                }
+                else
+                {
+                    Y.BackColor = Color.White;
+                }
+                if (ColorText.Text == "" || ColorText.Text == Properties.Resources.ColorTxt)
+                {
+                    ColorText.BackColor = Color.Red;
+                    Errors += "Incorrect Color";
+                }
+                else
+                {
+                    ColorText.BackColor = Color.White;
+                }
+                MessageBox.Show(Errors, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+
+            }
+            GC.Collect();
+        }
+    }
+}
